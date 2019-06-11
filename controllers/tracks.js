4d5a1203b0fdd0549edf1f273a29bdd0b6ac55ac
Michael Phillips <sqlscholar@gmail.com>
2019-06-11 14:10:27 -0400

    Set up API to consume track upload

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -11,9 +11,15 @@ const { Track } = require("../db/db");
 // };
 
 exports.postTrack = async (req, res) => {
+  console.log(req.body);
+  console.log(req.body.eventYear);
   const track = {
     title: req.body.title,
-    event: req.body.event
+    speaker: req.body.speaker,
+    event: req.body.event,
+    eventYear: req.body["event-year"],
+    eventMonth: req.body["event-month"],
+    eventDay: req.body["event-day"]
   };
   const results = await Track.create(track);
   res.status(201).json(results.dataValues);
--- models/track.js
+++ models/track.js
@@ -12,8 +12,24 @@ const Track = sequelize.define("track", {
     type: Sequelize.STRING(100),
     allowNull: false
   },
+  speaker: {
+    type: Sequelize.STRING(100),
+    allowNull: true
+  },
   event: {
     type: Sequelize.STRING(100),
+    allowNull: true
+  },
+  eventYear: {
+    type: Sequelize.SMALLINT,
+    allowNull: true
+  },
+  eventMonth: {
+    type: Sequelize.TINYINT,
+    allowNull: true
+  },
+  eventDay: {
+    type: Sequelize.TINYINT,
     allowNull: true
   }
 });
