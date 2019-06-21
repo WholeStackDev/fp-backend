00935deaaf5721642fd749bcb1a65bc04bf31a15
Michael Phillips <sqlscholar@gmail.com>
2019-06-21 11:45:35 -0400

    Changed DB Environment variable

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.HEROKU_POSTGRES_DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
   port: 5432,
